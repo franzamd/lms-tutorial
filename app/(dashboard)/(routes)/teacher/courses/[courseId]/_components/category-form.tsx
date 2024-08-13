3199209b1fa3dbd4e055e114199ad138fcc8d073
Franz Medrano <franz.medrano@icloud.com>
2024-08-13 12:00:58 -0400

    Course Publish Action

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form.tsx
@@ -54,10 +54,10 @@ export const CategoryForm = ({
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       await axios.patch(`/api/courses/${courseId}`, values);
-      toast.success('Course updated');
+      toast.success('Course updated successfully');
       toggleEdit();
       router.refresh();
-    } catch (error) {
+    } catch {
       toast.error('Something went wrong');
     }
   };
